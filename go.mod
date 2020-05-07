6445471a56529994ce7557a2523dd7b15982a1dd
maozheng <maozheng@maozhengdeMacBook-Pro.local>
2020-05-07 14:23:55 +0800

    快速上传

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/PlagueCat-Miao/GOIPFS
 
 go 1.13
+
+require (
+	github.com/PlagueCat-Miao/TheGoApiLerarnNote v0.0.0-20200507062015-2bc4fd0c0939 // indirect
+	github.com/plaguecat-miao/TheGoRpcLerarnNote v0.0.0-20200507044741-dabf8653b5bf // indirect
+)
--- server.go
+++ server.go
@@ -3,7 +3,7 @@ package main
 import (
 	// TheGoApiLerarnNote 项目有go.mod import必须与 module 相同
 	"github.com/PlagueCat-Miao/TheGoApiLerarnNote/GINtest"
-	// 只要TheGoRpcLerarnNote 项目里没go.mod 路径大小写都行
+	// 只要TheGoRpcLerarnNote 项目里没go.mod 路径大小写都行 go get 会在 pkg 自动写好路径 ，且你不要管带不带！ 大不大写
 	"github.com/plaguecat-miao/TheGoRpcLerarnNote/a"
 )
 
