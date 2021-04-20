aac79127c80bb0f49954ce373ef0f9c955de2b89
PlagueCat-Miao <ace0413@126.com>
2021-04-20 11:23:20 +0800

    补充单测

--- Db/MySql/MySqlUp.go
+++ Db/MySql/MySqlUp.go
@@ -8,12 +8,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 
-
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
+
 func init() {
-	fmt.Println("主MySql节点上线");
+	fmt.Println("主MySql节点上线")
 }
 
 //不大写，不public
@@ -21,90 +21,84 @@ type Login struct {
 	User     string `form:"user" json:"user" xml:"user" binding:"required" `
 	Password string `form:"password" json:"password" xml:"password" binding:"required" `
 	Targetdb string `form:"targetdb" json:"targetdb" xml:"targetdb" binding:"required"`
-	Dhash string `form:"Dhash" json:"Dhash" xml:"Dhash" binding:"required"`
-	Status string `form:"status" json:"status" xml:"status" binding:"required"`
-	Ip string `form:"ip" json:"ip" xml:"ip" binding:"required"`
+	Dhash    string `form:"Dhash" json:"Dhash" xml:"Dhash" binding:"required"`
+	Status   string `form:"status" json:"status" xml:"status" binding:"required"`
+	Ip       string `form:"ip" json:"ip" xml:"ip" binding:"required"`
 	Capacity string `form:"capacity" json:"capacity" xml:"capacity" binding:"required"`
-	Remain string `form:"remain" json:"remain" xml:"remain" binding:"required"`
-
+	Remain   string `form:"remain" json:"remain" xml:"remain" binding:"required"`
 }
-
 
 func test1(c *gin.Context) {
 
 	var json4 Login
 	if err := c.ShouldBindJSON(&json4); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err })
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		return
 	}
-	ClientIP:= c.ClientIP()
+	ClientIP := c.ClientIP()
 
-	c.JSON(http.StatusOK,gin.H{
-		"code":200,
-		"targetdb" : json4.Targetdb,
-		"Dhash" : json4.Dhash,
-		"status" : json4.Status,
-		"ip" : json4.Ip,
-		"ClientIP" : ClientIP,
-		"capacity" : json4.Capacity,
-		"remain" : json4.Remain,
-		"success":true,
-		})
-
+	c.JSON(http.StatusOK, gin.H{
+		"code":     200,
+		"targetdb": json4.Targetdb,
+		"Dhash":    json4.Dhash,
+		"status":   json4.Status,
+		"ip":       json4.Ip,
+		"ClientIP": ClientIP,
+		"capacity": json4.Capacity,
+		"remain":   json4.Remain,
+		"success":  true,
+	})
 
 }
-
 
 func main() {
-		router := gin.Default()
+	router := gin.Default()
 
-		// 此规则能够匹配/user/john这种格式，但不能匹配/user/ 或 /user这种格式
-		//可以获取/:xxxx处的路径名称，并跳转至/xxxx
-		router.GET("/user/:name", func(c *gin.Context) {
-			name := c.Param("name")
-			c.String(http.StatusOK, "Hello %s", name) //以字符串的形式回复
-		})
+	// 此规则能够匹配/user/john这种格式，但不能匹配/user/ 或 /user这种格式
+	//可以获取/:xxxx处的路径名称，并跳转至/xxxx
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name) //以字符串的形式回复
+	})
 
-		// 但是，这个规则既能匹配/user/john/格式也能匹配/user/john/send这种格式
-		// 如果没有其他路由器匹配/user/john，它将重定向到/user/john/
-	    //可以获取/:xxxx/*yyyy处的路径名称，并跳转至/xxxx
-		router.GET("/user/:name/*action", func(c *gin.Context) {
-			name := c.Param("name")
-			action := c.Param("action")
-			message := name + " is " + action
-			c.String(http.StatusOK, message)
-		})
-		router.POST("/do/test1",test1)
+	// 但是，这个规则既能匹配/user/john/格式也能匹配/user/john/send这种格式
+	// 如果没有其他路由器匹配/user/john，它将重定向到/user/john/
+	//可以获取/:xxxx/*yyyy处的路径名称，并跳转至/xxxx
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+	router.POST("/do/test1", test1)
 
-		router.Run(":8080")
-
+	router.Run(":8080")
 
 }
-
 
 func Add_MYSQL() {
 	conn, err := sql.Open("mysql", "root:12345678@tcp(localhost:3306)/ipfs_netserver")
 	if nil != err {
 		fmt.Println("connect db error: ", err)
 	}
-	targetdb :="cloud"
+	targetdb := "cloud"
 	Dhash := "QmABC"
 	status := "3"
 	ip := "127.0.0.1"
-	capacity :="400"
-	remain :="300" ;
+	capacity := "400"
+	remain := "300"
 
 	var SET_VAL string
-	SET_VAL="('"+ Dhash +"','"+status+"','"+ip+"','"+capacity+"','"+remain+"') "
-	fmt.Println(targetdb +" 插入: "+ SET_VAL);
+	SET_VAL = "('" + Dhash + "','" + status + "','" + ip + "','" + capacity + "','" + remain + "') "
+	fmt.Println(targetdb + " 插入: " + SET_VAL)
 
-	_ , err =  conn.Query("INSERT INTO `ipfs_netserver`.`"+ targetdb +"` VALUES " +SET_VAL)
-	if nil != err{
+	_, err = conn.Query("INSERT INTO `ipfs_netserver`.`" + targetdb + "` VALUES " + SET_VAL)
+	if nil != err {
 		fmt.Println("query db insert error: ", err.Error())
 		return
 	}
-	err =conn.Close();
-	if nil != err{
+	err = conn.Close()
+	if nil != err {
 		fmt.Println("close db error: ", err.Error())
 		return
 	}
--- Db/MySql/gorm_mysql_test.go
+++ Db/MySql/gorm_mysql_test.go
@@ -2,8 +2,8 @@ package MySql
 
 import (
 	"fmt"
-	"github.com/jinzhu/gorm"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
 
@@ -11,28 +11,26 @@ import (
 )
 
 func init() {
-	log.Println("Gorm连接数据库MySql 测试");
+	log.Println("Gorm连接数据库MySql 测试")
 }
+
 //由于grom是使用的orm映射，所以需要定义要操作的表的model，在go中需要定义一个struct， struct的名字就是对应数据库中的表名
 //targetdb string
 
 //对应的字段名的查找会先按照tag里面的名称去里面查找，如果没有定义标签则按照struct定义的字段查找，查找的时候struct字段中的大写会被转义成“ ”,例“GroupZone”会去查找表中的group_zone字段
 //https://www.jb51.net/article/151051.htm
 type Cloud struct {
-
 	Dhash string `gorm:"column:Dhash;primary_key"` //一定要声明主键
-													//1。 首先gorm是不知道主键、外键等tag的，很多操作可能导致gorm语法级别的err
-													//删除Delete、修改Model.Update都是仅参考输入的主键，（剩下的靠Where()）
-													//如果声明struct时未声明 `gorm:"primary_key"` 那即使次字段是数据库的主键
-													// gorm 也会认为没有主键，而导致全选（相当于Model($xx{}).update）
-	Status int //此处必须大写，但是没写tag,所以gorm会转义为小写
-	Ip string
+	//1。 首先gorm是不知道主键、外键等tag的，很多操作可能导致gorm语法级别的err
+	//删除Delete、修改Model.Update都是仅参考输入的主键，（剩下的靠Where()）
+	//如果声明struct时未声明 `gorm:"primary_key"` 那即使次字段是数据库的主键
+	// gorm 也会认为没有主键，而导致全选（相当于Model($xx{}).update）
+	Status   int //此处必须大写，但是没写tag,所以gorm会转义为小写
+	Ip       string
 	Capacity int
-	Remain int
+	Remain   int
 	//gorm.Model //这个在删除的时候用
 }
-
-
 
 func TestGorm_CDUR(t *testing.T) {
 	t.Log("Hellcat: gorm-MySql测试")
@@ -46,58 +44,57 @@ func TestGorm_CDUR(t *testing.T) {
 
 	db.SingularTable(true) //让grom转义struct名字的时候不用加上s//https://www.jb51.net/article/151051.htm
 	fmt.Println("单数表模式指，首字母大写的struct 不会")
-	fmt.Println("结构体Cloud是否连接上MySql表cloud ",db.HasTable(Cloud{}))
+	fmt.Println("结构体Cloud是否连接上MySql表cloud ", db.HasTable(Cloud{}))
 	//db=db.Table("cloud")
 	// 自动迁移模式
 	//gorm可以支持自动迁移,也就是自动的表结构迁移,只会创建表,补充缺少的列,缺少的索引。
 	//db.AutoMigrate(&Cloud{})
 
 	// 读取Read
-	var cadd,cadd2 Cloud
+	var cadd, cadd2 Cloud
 	err = db.Where("Dhash = ?", "QmGorm_新建喵").Find(&cadd).Error
 
 	//db.First(&cadd2, 1) // 获取第一条数据,可以接在任何DB后(如where().First)，也可以自己写一些条件
-	db.First(&cadd2 ,"Dhash = ?", "QmGorm_新建喵") // 获取第一条数据,可以接在任何DB后(如where().First)，也可以自己写一些条件
+	db.First(&cadd2, "Dhash = ?", "QmGorm_新建喵") // 获取第一条数据,可以接在任何DB后(如where().First)，也可以自己写一些条件
 	// 查询code为l1212的product
 
-    log.Println(cadd2)
+	log.Println(cadd2)
 
 	// 更新Update - 更新数据库
-	err =db.Model(&cadd2).Updates(map[string]interface{}{"ip" : "127.0.0.1:3306","Capacity" :500 }).Error
+	err = db.Model(&cadd2).Updates(map[string]interface{}{"ip": "127.0.0.1:3306", "Capacity": 500}).Error
 	// UPDATE Cloud SET ip='127.0.0.1:3306', Capacity=500 WHERE Dhash = kkk;(这是因为主键)
-	err =db.Model(&Cloud{}).Update("remain",140 ).Error
+	err = db.Model(&Cloud{}).Update("remain", 140).Error
 	// UPDATE Cloud SET remain=140 (这是因为主键)
 	// 你要是想改主键 ，就删掉重新建立
 	db.Delete(&cadd2)
-	cadd2.Dhash="Qm一定要改回主键喵"
+	cadd2.Dhash = "Qm一定要改回主键喵"
 	db.Save(&cadd2)
-    if err != nil{
-		log.Println("更新失败： ",err)
+	if err != nil {
+		log.Println("更新失败： ", err)
 		fmt.Println("这种失败会导致 go的struct数据，改变而数据库未改变,一定要注意err的返回啊")
 
-	}else {
-		log.Println("Update success\n",">>>>>> A\n", cadd, "\n======\n", cadd2, "\n>>>>>> B")
+	} else {
+		log.Println("Update success\n", ">>>>>> A\n", cadd, "\n======\n", cadd2, "\n>>>>>> B")
 		fmt.Println("即使数据库成update功,go的struct旧副本没有更新（A:cadd B:cadd2）,一定要注意用最新的副本")
 	}
 
 	// 创建 Create
 	db.Create(&Cloud{
 
-		Dhash : "QmGorm_新建喵",
-		Status : 3,
-		Ip : "127.0.0.1",
-		Capacity :400,
-		Remain :300 })
+		Dhash:    "QmGorm_新建喵",
+		Status:   3,
+		Ip:       "127.0.0.1",
+		Capacity: 400,
+		Remain:   300})
 	db.Save(&Cloud{
 
-		Dhash : "QmGorm_+w覆盖喵",
-		Status : 3,
-		Ip : "127.0.0.2",
-		Capacity :400,
-		Remain :300 })
+		Dhash:    "QmGorm_+w覆盖喵",
+		Status:   3,
+		Ip:       "127.0.0.2",
+		Capacity: 400,
+		Remain:   300})
 
 	//删除 Delete  - 删除product
 	//db.Delete(&cadd2)
 	t.Log("Hellcat: MySql 测试结束")
 }
-
